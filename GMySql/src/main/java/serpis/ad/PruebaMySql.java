eadfef7be084022553f2ce0a5d993c85db10599f
davidmp96 <2dam.dmunoz@institutoserpis.org>
2017-11-29 10:06:37 +0100

    GMySql finalizado, con conexion a la base de datos y con preparedStatement para visualizar los datos de articulo

--- GMySql/src/main/java/serpis/ad/PruebaMySql.java
+++ GMySql/src/main/java/serpis/ad/PruebaMySql.java
@@ -2,29 +2,38 @@ package serpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class PruebaMySql {
 
-	public static void main(String[] args) throws SQLException {
-
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba", 
-				"root", 
-				"sistemas");
-	
-		java.sql.Statement stmt = connection.createStatement();
-		
-	    ResultSet rs = stmt.executeQuery( "SELECT * FROM categoria");
-	    
-	    while ( rs.next() ) {
-	        int numColumns = rs.getMetaData().getColumnCount();
-	        for ( int i = 1 ; i <= numColumns ; i++ ) {
-	           System.out.println( "COLUMN " + i + " = " + rs.getObject(i) );
-	        }
-	    }
-		connection.close();
-	} 	
+  public static void main(String[] args) throws SQLException {
+    
+    Connection connection = DriverManager.getConnection(
+				    		"jdbc:mysql://localhost/dbprueba",
+				    		"root",
+				    		"sistemas");
+      
+    PreparedStatement preparedStatement=connection.prepareStatement("select * from articulo where id > ?");
+    preparedStatement.setObject(1, 6);
+    ResultSet resultSet=preparedStatement.executeQuery();
+    
+      System.out.printf("%5s %-30s %10s %9s\n", "id", "nombre", "precio", "categoria");
+    
+      while(resultSet.next()){
+    	  System.out.printf("%5s %-30s %10s %9s\n",
+	      resultSet.getObject("id"),
+	      resultSet.getObject("nombre"),
+	      resultSet.getObject("precio"),
+	      resultSet.getObject("categoria"));
+	      
+    }
+      preparedStatement.close();
+      connection.close();
+      System.out.println("Fin");
+  }
 }
 
