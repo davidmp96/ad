f157212100166ee8381de9a299bec4f10b83109f
davidmp96 <2dam.dmunoz@institutoserpis.org>
2017-12-04 13:45:51 +0100

    Añadida segunda versión del GArticulo, implementado update en ArticuloDao y realizando pruebas en ArticuloMain. Quedando por realizar ArticuloView y Menú en ArticuloMain.

--- GArticulo/src/main/java/serpis/ad/ArticuloMain.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloMain.java
@@ -1,14 +1,58 @@
 package serpis.ad;
 
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ArticuloMain {
 	
 	public enum Option{Salir, Nuevo, Editar, Eliminar, Consultar, Listar};
+	public enum State{Vacio, Medio, Lleno};
 	
 	private static Scanner scanner = new Scanner(System.in);
+	
+	public static class Articulo {
+		private long id;
+		private String nombre;
+		private BigDecimal precio;
+		private long categoria;
+	}
+	
 	public static void main(String[] args) {
-		scanOption("Opcion (0-5): ", "^[012345]$");
+		
+		showFields(Articulo.class);
+		
+//		new Menu.add("Salir", null)
+//			.add("Nuevo", () -> nuevo())
+//			.add("Editar",() -> editar())
+//			.run();
+//		
+//	}
+	
+	}
+	
+	private static void showFields(Class<?> type) {
+		for(Field field : type.getDeclaredFields() )
+			System.out.printf("%s %s\n", field.getName(), field.getType().getName());
+	}
+	
+	public static void nuevo() {
+		///TODO implementar
+	}
+		
+	public static <T extends Enum<T>> T scan(Class<T> enumType) {
+		T[] constants = enumType.getEnumConstants();
+		for(int index  = 0; index < constants.length; index++)
+			System.out.printf("%s - %s\n", index, constants[index]);
+		String options = String.format("^[0-%s]$", constants.length - 1);
+		while (true) {
+			System.out.println("Elige una opción: ");
+			String line = scanner.nextLine();
+			if (line.matches(options))
+				return constants[Integer.parseInt(line)];
+			System.out.println("Opcion invalida. Vuelve a introducir");
+			
+		}
 	}
 		
 	public static int scanInt(String label) {
--- GArticulo/src/main/java/serpis/ad/ArticuloView.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloView.java
@@ -3,6 +3,5 @@ package serpis.ad;
 import java.util.Scanner;
 
 public class ArticuloView {
-	static Scanner scanner = new Scanner(System.in);
 	
 }
